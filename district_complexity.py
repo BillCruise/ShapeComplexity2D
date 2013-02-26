a5c7ba9d65fcdf3fce32fb659757600054b5f20f
Bill Cruise <lizard.bill@gmail.com>
2013-02-26 09:47:36 -0500

    Only compute the complexity of the largest polygon in a district.
    Computing the complexity of all 3300+ shapes was time consuming and
    unnecessary.  I added an initial pass through the shape file to find the
    polygon with the largest area in each district.

--- district_complexity.py
+++ district_complexity.py
@@ -3,6 +3,7 @@
 
 import json
 from shape_complexity_2d.shape_complexity_2d import *
+from shape_complexity_2d.centroid import area
 
 print('Opening data file...')
 json_data = open('data/cgd112p020-AK00.json')
@@ -20,7 +21,10 @@ print('Data Type:', dtype)
 f_count = len(features)
 print('Features:', f_count)
 
-
+# create an empty dictionary where the key is 'state+district'
+# and the value is the shape for that district with the
+# maximum area.
+primary_land_masses = {}
 
 # create an empty dictionary where the key is 'state+district'
 # and the value is the district complexity.
@@ -29,7 +33,6 @@ district_complexity = {}
 count = 1
 
 for feature in features:
-    
     properties = feature['properties']
     state = properties['STATE']
     cong_district = properties['CONG_DIST']
@@ -42,19 +45,47 @@ for feature in features:
     coordinates = geometry['coordinates'][0]
     print 'Coordinates:', len(coordinates)
 
-    complexity = shape_complexity(coordinates)
-
-    print 'Complexity:', complexity
+    # complexity = shape_complexity(coordinates)
+    # print 'Complexity:', complexity
+    
+    a = area(coordinates)
+    print 'Area:', a
+    
     print
 
     if state != None and cong_district != None:
-        if (state + ':' + cong_district) in district_complexity:
-            if district_complexity[state + ':' + cong_district] < complexity:
-                district_complexity[state + ':' + cong_district] = complexity
+        if (state + ':' + cong_district) in primary_land_masses:
+            if primary_land_masses[state + ':' + cong_district][0] < a:
+                primary_land_masses[state + ':' + cong_district] = [a, coordinates]
         else:
-            # add the coordinates for the new district
-            district_complexity[state + ':' + cong_district] = complexity
+            # add the [area, coordinates] for the new district
+            primary_land_masses[state + ':' + cong_district] = [a, coordinates]
 
+count = 1
+total = len(primary_land_masses)
+
+print
+print 'Calculating complexity of primary district shapes...'
+for key, value in primary_land_masses.iteritems():
+    print count, 'of', total
+    print key, value[0]
+    print 'First Coordinate:', value[1][0]
+    complexity = shape_complexity(value[1])
+    district_complexity[key] = complexity
+    print 'Complexity:', complexity
+    print
+    count += 1
+            
+
+    #if state != None and cong_district != None:
+    #    if (state + ':' + cong_district) in district_complexity:
+    #        if district_complexity[state + ':' + cong_district] < complexity:
+    #            district_complexity[state + ':' + cong_district] = complexity
+    #    else:
+    #        # add the coordinates for the new district
+    #        district_complexity[state + ':' + cong_district] = complexity
+
+print
 sorted_districts = sorted(district_complexity, key=district_complexity.get)
 
 for district in sorted_districts:
